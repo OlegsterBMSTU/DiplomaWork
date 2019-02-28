1c35f3e948e75fe99e0d88d8fcb3d7a4d3b9b232
OlegsterBMSTU <mailolegster@gmail.com>
2019-03-01 00:10:54 +0300

    Last changes from 28th of february. Changed lookingForFunction and matrixWork_new_function

--- lookingForFunction.cpp
+++ lookingForFunction.cpp
@@ -59,7 +59,7 @@ void fillingArrays() {
 	int myFileSize = 0;
 	fileNameOut = "matrixWork" + toString(i + 6) + ".txt";	//Выходной файл 196*196 элементов
 	fileNameIn = "matrixWork" + toString(i) + ".txt";		// входной файл 3600*3600
-	fileNameIntermediate = "exit_convolusion_" + toString(i) + ' ' + toString(1) + ".txt";
+	fileNameIntermediate = "exit_convolusion_" + toString(i) + ' ' + toString(1) + ".txt"; // Задано статично 0.1 для тестов и проверки. Потом переделать чтобы по координатам нормально работало
 	string pointer;	//указатель на элемент, который считваем
 	/* Создаем массивы,куда считаем данные из файлов для поиска элементов */
 	/* Элемент запишем в отдельную структуру, где будут храниться индексы*/
@@ -90,88 +90,85 @@ void fillingArrays() {
 	ifstream inFile(fileNameIn);
 
 	// Записываем данные из файлов в массивы
+	//Заполняем массив выходящий
+	// Переделал функции на while для нормальной работы с файлом
 	myFileSize = sqrt(sizeFile(fileNameOut));
 	testFile << "\nExit File:" << endl << endl;
 	while (getline(exitFile, pointer)) {
 		exitDataArray[i][j] = fromString<float>(pointer);
-		testFile << exitDataArray[i][j] << endl;
+		testFile << exitDataArray[i][j] << "       ";
 		j++;
 		if (j == myFileSize) {
 			i++;
 			j = 0;
+			testFile<<endl;
 		}
 	}
 
 
+	//Заполняем массив средний
 	myFileSize = sqrt(sizeFile(fileNameIntermediate));
 	i = 0;
 	testFile << "\nMiddle File:" << endl << endl;
 	while (getline(iterFile, pointer)) {
 		intermediateDataArray[i][j] = fromString<float>(pointer);
-		testFile << intermediateDataArray[i][j] << endl;
+		testFile << intermediateDataArray[i][j] << "       ";
 		j++;
 		if (j == myFileSize) {
 			i++;
 			j = 0;
+			testFile<<endl;
 		}
 
 	}
 
-
+	//Заполняем массив Входной
 	myFileSize = sqrt(sizeFile(fileNameIn));
 	//	 testFile << "In File:" <<endl<<endl;
-
-
-
 	i = 0;
 	while (getline(inFile, pointer)) {
 		inDataArray[i][j] = fromString<float>(pointer);
-		testFile << inDataArray[i][j] << endl;
+		testFile << inDataArray[i][j] << "       ";
 		j++;
 		if (j == myFileSize) {
 			i++;
 			j = 0;
+			testFile<<endl;
 		}
 	}
 
 }
 
-	//Эта функция будет искать коордитнаты значения в главном массиве
-	void lookingForCoordinatesInTheMiddleArray() {
-		// Будем пробовать искать координату в интермедиэйт массиве исходя из из данных в выходном массиве
-		// и будем сохранять в структуру 
-		testFile << endl << endl;
-		testFile << "void lookingForCoordinatesInTheMiddleArray" << endl;
-		int myFileSizeOut = sqrt(sizeFile(fileNameOut));		//храним размер массива выходного файла
-		int myFileSizeIntermediate = sqrt(sizeFile(fileNameIntermediate));	// храним размер массива среднего файла
-		for (int i = 0; i < myFileSizeOut; i++) {
-			for (int j = 0; j < myFileSizeOut; j++) {
-				for (int x = i * 4; x < i * 4 + 4; x++) {
-					/* от элемента i*4 до i*4+4 для того что i однозначно определяет в каком
+//Эта функция будет искать коордитнаты значения в главном массиве
+// Пока не работает. Заполняет первые 14 значений, далее беда. Бум думать
+void lookingForCoordinatesInTheMiddleArray() {
+	// Будем пробовать искать координату в интермедиэйт массиве исходя из из данных в выходном массиве
+	// и будем сохранять в структуру 
+	testFile << endl << endl;
+	testFile << "void lookingForCoordinatesInTheMiddleArray" << endl;
+	int myFileSizeOut = (sizeFile(fileNameOut));		//храним размер массива выходного файла
+	int myFileSizeIntermediate = sqrt(sizeFile(fileNameIntermediate));	// храним размер массива среднего файла
+	for (int i = 0; i < myFileSizeOut; i++) {
+		for (int j = 0; j < myFileSizeOut; j++) {
+			for (int x = i * 4; x < i * 4 + 4; x++) {
+				/* от элемента i*4 до i*4+4 для того что i однозначно определяет в каком
+				диапазоне будет находится координата в среднем массиве */
+				for (int y = j*4; y < j*4+4; y++) {
+					/* от элемента j*4 до j*4+4 для того что j однозначно определяет в каком
 					диапазоне будет находится координата в среднем массиве */
-					for (int y = 0; y < myFileSizeIntermediate; y++) {
-						/* от элемента j*4 до j*4+4 для того что j однозначно определяет в каком
-						диапазоне будет находится координата в среднем массиве */
-						if (exitDataArray[i][j] == intermediateDataArray[x][y]) { //Если элемент выходного массива найден в среднем
-							coordinates[i].i = x;	//тогда сохраняем координату i
-							coordinates[i].j = y;   // и координату j
-						}
+					if (exitDataArray[i][j] == intermediateDataArray[x][y]) { //Если элемент выходного массива найден в среднем
+						coordinates[i].i = x;	//тогда сохраняем координату i
+						coordinates[i].j = y;   // и координату j
 					}
 				}
 			}
 		}
-
-
-		for (int i = 0; i < 196; i++) {
-			testFile << coordinates[i].i << " " << coordinates[i].j << endl;
-		}
-		testFile.close();
 	}
 
-	int control(int i, int j, int myFileSize) {
-		if (j == myFileSize) {
-			i++;
-			j = 0;
-		}
-		return i, j;
+
+	for (int i = 0; i < 196; i++) {
+		testFile << coordinates[i].i << " " << coordinates[i].j << endl;
 	}
+	testFile.close();
+}
+
--- matrixWork_new_function.cpp
+++ matrixWork_new_function.cpp
@@ -11,10 +11,12 @@
 using namespace std;
 float ** matrixWork;
 int myFileSize;
-float max = 0, current = 0;
-int aj=0,ai=0,counter=0;
-bool key = true;
-bool keyForI = false;
+float current = 0;
+int aj = 0, ai = 0, countBitch = 0;
+bool key0 = false, key1 = false, key2 = 0;
+
+
+string from_s1;
 
 template<typename T>
 T fromString(const std::string& s)
@@ -32,6 +34,9 @@ std::string toString(T val)
 	return oss.str();
 }
 
+void maxFromArray(int i, int j);
+void writeMaxInFile(float max);
+
 void subSampling(int index1, int index2, int selector)
 {
 	string line;	//указатель, который считвает
@@ -43,29 +48,30 @@ void subSampling(int index1, int index2, int selector)
 	string file = from_s;	//Название файла с которым работает
 
 	myFileSize = sqrt(sizeFile(file));
-	matrixWork= new float * [myFileSize];	//матрица для формирования выходных данных
-		for (int i=0;i<myFileSize;i++) matrixWork[i] = new float [myFileSize];
+	matrixWork = new float *[myFileSize];	//матрица для формирования выходных данных
+	for (int i = 0; i < myFileSize; i++) matrixWork[i] = new float[myFileSize];
 	int k = 0, i = 0;
 	//while (i < (myFileSize)) matrixWork[i++] = 0; //Обнуляем всю матрицу
-	for (int i=0;i<myFileSize;i++) {
-		for (int j=0;j < myFileSize;j++) {
+	for (int i = 0; i < myFileSize; i++) {
+		for (int j = 0; j < myFileSize; j++) {
 			matrixWork[i][j] = 0;
 		}
 	}
-	
+
 	i = 0; k = 0;
-	
+
 	ifstream f1(file);
 
 	// Название выходного файла с данными
 	// с учетом его индекса [0..11] или [1..12]
-	string from_s1("matrixWork");
+	from_s1= "matrixWork";
 	from_s1 += (toString(selector)) + ".txt";
-
 	ofstream f2(from_s1);
+	
+	
 
 	// Цикл выбора максимальных (ReLu) элементов // 
-	while (getline(f1, line)) {
+	/*	while (getline(f1, line)) {
 		current = fromString <float>(line);
 		if ((max > current) || (key)) max = current;	//Выбор максимального
 		key = false;
@@ -77,44 +83,102 @@ void subSampling(int index1, int index2, int selector)
 			key = true;
 			i = 0;
 		}
-	}
-	f1.close(); f2.close();
-	delete[] matrixWork;
-	
-	for (int i =0; i< myFileSize; i++) {
-		for (int j=0; j<myFileSize; j++) {
-			maxFromArray(i,j);
+	}*/
+	// Заполняем матрицу matrixWork
+	i = 0; int j = 0;
+	while (getline(f1, line)) {
+		matrixWork[i][j] = fromString<float>(line);
+		//f2 << matrixWork[i][j] << endl;
+		j++;
+		if (j == myFileSize) {
+			j = 0; i++;
 		}
 	}
-	
+	f1.close(); 
+	int length = sqrt(sizeFile(from_s));
+	int steps = ((sizeFile(from_s)) / 16);	//потому то входной файл из 3136 / 16 есть результат последнего файла
+	int ai = 0, aj = 0;
+	k = 0;
+	i = 0; j = 0;
+	float * max = new float[length];
+	bool key = true;
+	while ((k < steps) && (j!=length))
+	{
+		if (key) {
+			max[k] = matrixWork[i][j];
+			key = false;
+		}
+		(matrixWork[i][j] > max[k]) ? max[k] = matrixWork[i][j] : 0;
+		
+		if ((j + 1) % 4 == 0) {	//когда достигли 4 ой ячейки по горизонтали в карте 4х4
+			j = aj;	//сбросить j
+			key0 = true;
+		}
+		if (((j + 1) % 4 == 0) && ((i + 1) % 4 == 0)) {  //когда карту 4х4 прохавали до конца, т.е. i=4,j=4
+			i = ai;
+			j++;
+			aj += 4;
+			k++;			// подняли итератор
+			key = true;		//для разрешения забись для сравнения в новом наборе данных
+		}
+		if ((j == length) && ((i+1)%4) ==0 ) { //когда достигли последней карты 4х4 в строке матрицы
+			i++;		//передвинули i на следующую строку
+			j = 0;		// поставили индекс j в начало
+			ai += 4;
+		}
+		if (!key0) j++;
+		if (key0) {
+			i++;
+			key0 = 0;
+		}
+	}
+
+	for (int i = 0; i < steps; i++) {
+		f2 << toString(max[i]) << endl;
+	}
+
+	//delete[] matrixWork;
 }
 
 
+/*
 //Новаz функуия для двумерного массива, проходим картой по массиву
-void maxFromArray(int i; int j) {
+void maxFromArray(int i, int j) {
 	current = matrixWork[i][j];
 	if ((max > current) || (key)) max = current;
-	if ((j==myFileSize)&& (i == myFileSize-ai)) {
+	if ((j == myFileSize) && (i == myFileSize - ai)) {
 		keyForI = true;  // поднимаем ключ для перехода на новые i
 	}
-
-	key=false;
-	if ((j == aj+4)) {
-		j=0;		//Если в квадрате 4х4 закончили строчку то j сбросить в 0
+	key = false;
+	if ((j == aj + 4)) {
+		j = 0;		//Если в квадрате 4х4 закончили строчку то j сбросить в 0
 		i++;		//надо оставить чтобы в основную функцию вернул значение
-		counter++;	//повысить счечик для контроля прохода по окну
+		countBitch++;	//повысить счечик для контроля прохода по окну
 	}
-	if ((counter ==4) && (j == aj+4)) { // если прошли карту 4х4 до конца
-		counter=0;		//сбрасываем счетчик
-		aj = aj+4;		//повышаем указатель для j на 4 для прохода по следующей карте
+	if ((countBitch == 4) && (j == aj + 4)) { // если прошли карту 4х4 до конца
+		countBitch = 0;		//сбрасываем счетчик
+		aj = aj + 4;		//повышаем указатель для j на 4 для прохода по следующей карте
+		keyForMax = true;
 	}
-	if ((i == ai+4) && (j == aj+4)) {  //если закончили по i и по j, то есть прошли строку шириной в 4i до кноца
-		ai+=4;		//увеличиваем ai на 4 для работы со следующим набором i
-		j=0;		//на всякий пожарный. В теории когда (i == 3 ) && (j == 14) для первой строки j сбросится в 0 по условиям основного цикла
+	if (keyForMax) {
+		writeMaxInFile(max);
+		keyForMax = 0;
 	}
-	
-	if (i==ai+4) i=ai;	//если закончили по i, но не закончили по j то i сбрасываем, точнее присваиваем +4
-	if ((i == ai+4) && (j == myFileSize)) i+=4;	//если достигли конца матрицы по горизонтали (i=4, j=sizeArray) то переходим на следующую строчку для поиска
+	if ((i == ai + 4) && (j == aj + 4)) {  //если закончили по i и по j, то есть прошли строку шириной в 4i до кноца
+		ai += 4;		//увеличиваем ai на 4 для работы со следующим набором i
+		j = 0;		//на всякий пожарный. В теории когда (i == 3 ) && (j == 14) для первой строки j сбросится в 0 по условиям основного цикла
+	}
+
+	if (i == ai + 4) i = ai;	//если закончили по i, но не закончили по j то i сбрасываем, точнее присваиваем +4
+	if ((i == ai + 4) && (j == myFileSize)) i += 4;	//если достигли конца матрицы по горизонтали (i=4, j=sizeArray) то переходим на следующую строчку для поиска
 	//продумать переход ai и aj
 }
 
+*/
+//вернуть 2 значения
+
+void writeMaxInFile(float max) {
+	ofstream f2(from_s1, ios::app);
+	f2 << max << endl;
+	f2.close();
+}
